9ee7bc0b82c80d7a6d3df9ccb3f4300097ca8f03
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-08-17 18:38:00 +0000

    updated app interface

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-
 import streamlit as st
 
-from api import Phi3VisionAI
+from src.api import client
+from src.config import MODEL, SYSTEM_MESSAGE
 from src.ui_components import file_upload, header
-
-# Define Phi3 model client
-client = Phi3VisionAI()
+from src.utils import prepare_content_with_images
 
 IMAGE_EXTENSIONS = {".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp"}
 VIDEO_EXTENSIONS = {".mp4", ".mkv", ".mov", ".avi", ".flv", ".wmv", ".webm", ".m4v"}
@@ -20,59 +18,69 @@ def main():
     # Title section
     header()
 
+    # Add input field for system prompt
+    st.sidebar.header("System Prompt")
+    system_prompt = st.sidebar.text_area(
+        label="Modify the prompt here.", value=SYSTEM_MESSAGE["content"]
+    )
+    SYSTEM_MESSAGE["content"] = system_prompt
+
     # Sidebar section for file upload
     uploaded_file, file_object = file_upload()
 
-    print(uploaded_file)
-    print(file_object.__len__())
-
-    # Load chat history from file
-    messages = None
-    if messages is None:
-        messages = []
-
     # Initialize chat history
     if "messages" not in st.session_state.keys():
-        st.session_state.messages = messages
+        st.session_state.messages = []
+
+    if "messages" in st.session_state.keys() and len(st.session_state.messages) > 0:
+        # add clear chat history button to sidebar
+        st.sidebar.button(
+            "Clear Chat History",
+            on_click=lambda: st.session_state.messages.clear(),
+            type="primary",
+        )
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
-        # skip if role is system
-        if message["role"] == "system":
-            continue
         # Display chat message in chat message container
         with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+            content = message["content"]
+            if isinstance(content, list):
+                st.markdown(content[0]["text"])
+                caption = "Thumbnail for Video" if len(content) > 2 else ""
+                st.image(
+                    content[1]["image_url"]["url"],
+                    width=200,
+                    caption=caption,
+                )
+            else:
+                st.markdown(content)
 
-    # React to user input
-    if prompt := st.chat_input(
-        "Ask something", disabled=uploaded_file is None, key="prompt"
-    ):
+    if prompt := st.chat_input("Ask something", key="prompt"):
         # Display user message in chat message container
         with st.chat_message("user"):
             st.markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
+            content = (
+                prepare_content_with_images(prompt, file_object)
+                if file_object
+                else prompt
+            )
+            if file_object:
+                caption = "Thumbnail of Video" if len(file_object) > 1 else ""
+                st.image(file_object[0]["image_url"]["url"], width=200, caption=caption)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": content})
 
-        # with st.spinner("Thinking..."):
-        # Placeholder function to send message to Phi3 model API
-        stream = client.chat(st.session_state.messages, file_object)
-
-        # Display Phi3 response in chat message container
+        # Get response from the assistant
         with st.chat_message("assistant"):
+            messages = [SYSTEM_MESSAGE, *st.session_state.messages]
+            stream = client.chat.completions.create(
+                model="llama-3.1-8b-instant", messages=messages, stream=True
+            )
             response = st.write_stream(stream)
-        # Add Phi3 response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
 
-    # Save chat history before closing the app
-    # save_chat_history(st.session_state.messages)
-
-    # Made with section
-    # st.markdown("---")
-    # st.markdown("Made with ❤️ by [Bhimraj Yadav](https://github.com/bhimrazy)")
-
-
-
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 if __name__ == "__main__":
