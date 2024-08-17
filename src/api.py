6b49c33c02a95efd1085f9813105f04b7ffe443a
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-08-17 17:54:00 +0000

    moves api snippet

---
+++ src/api.py
@@ -0,0 +1,7 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    # api_key="lit",
+    api_key="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",
+)
