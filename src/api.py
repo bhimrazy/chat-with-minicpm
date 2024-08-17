4e3be56efc9b1eb3bb3b15645abaa612436f48dd
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-08-17 18:39:02 +0000

    chore: Update API base URL and key

--- src/api.py
+++ src/api.py
@@ -1,7 +1,6 @@
 from openai import OpenAI
 
 client = OpenAI(
-    base_url="https://api.groq.com/openai/v1",
-    # api_key="lit",
-    api_key="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",
+    base_url="http://127.0.0.1:8000/v1",
+    api_key="lit",
 )
